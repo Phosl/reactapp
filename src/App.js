505702613fdbfd327a74fc091a1c8931f9263f93
Filippo de Gennaro <filippodegennaro@hotmail.it>
2019-08-04 15:05:58 +0200

    switch to functional component - react HOOKS preview

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-	render() {
+const App = props => {
+	const [personsState,setPersonsState] = useState ({
+			persons: [
+				{ name: 'Max', age: 28},
+				{ name: 'Mia', age: 12},
+				{ name: 'Rod', age: 27},
+			]
 
-		return (
-			// good to close in just one root element
-		    <div className="App"> 
-				<h1>Hi, i’m a React</h1>
-				
-				<Person />
-				<Person />
-				<Person />
-		    
-		    </div>
-		);
-// React.createElement('element',{className:'myclass'}, 'myText')
-// 	return React.createElement('div',{className:'App'}, React.createElement('h1', {className:'title'},'Hi, I’m React App!!!'))
-	}
+	}  );
+
+const [otherState, setOtherState]  = useState('some other value');
+
+
+	console.log(personsState,otherState)
+  
+
+	const switchNameHandler = () => {
+			console.log('Was Clicked!!!')
+			// DON'T DO THIS: this.state.persons[0].name = 'Maximilian'; USE:
+			setPersonsState({
+			persons: [
+				{ name: 'Maximilian', age: 28},
+				{ name: 'Mia', age: 12},
+				{ name: 'Rod', age: 99}
+			]
+			})		
+	} 
+
+
+
+	return (			
+		// good to close in just one root element
+	    <div className="App"> 
+			<h1>Hi, i’m a React</h1>
+			
+			<button onClick={switchNameHandler} >Switch Name</button>
+			
+			<Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+			<Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+			<Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+			
+			
+	    </div>
+	);
+		// React.createElement('element',{className:'myclass'}, 'myText')
+		// 	return React.createElement('div',{className:'App'}, React.createElement('h1', {className:'title'},'Hi, I’m React App!!!'))
 }
 
 export default App;
+
+
+	
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 
-// function name lower case
-const person = () => {
-	let name = 'Mark'
-	return <p>I’m a Person my name is {name}</p> 
+// Function component - name lower case
+const person = (props) => {
+	return (
+	<div>
+		<p>I’m {props.name} and I am {props.age} years old </p>
+		<p>{props.children}</p> 
+
+	</div>
+	) 
 };
 
 export default person;
