503459264ff8ccf49649677aaacf810f2aa7751f
filippodegennaro@hotmail.it <filippodegennaro@hotmail.it>
2019-08-23 21:11:27 +0200

    rollback

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,12 +5,6 @@ import classes from './Person.module.css';
 
 // Function component - name lower case
 const person = (props) => {
-
-	const rnd = Math.random();
-	if(rnd > 0.7) {
-		throw new Error('Cazzo!')
-	}
-
 	return (
 	<div className={classes.Person} >
 		<p onClick={props.click} >I’m {props.name} and I am {props.age} years old </p>
