8bd0833c6756ba49be22cf6d8659c8d825e3fc2d
filippodegennaro@hotmail.it <filippodegennaro@hotmail.it>
2020-01-05 08:52:13 +0100

    component flow

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,43 @@
-import React from "react";
-import Person from "./Person/Person";
+import React, {Component} from 'react'
+import Person from './Person/Person'
+
+class Persons extends Component {
+
+/*  we comment this becouse its initial state is undefined. */
+//   static getDerivedStateFromProps(props, state) {
+//     console.log('[Persons.js] getDerivedStateFromProps ')
+//     return state
+//   }
 
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person 
-        click={() => props.clicked(index)}
-        key={person.id}
-        name={person.name} 
-        age={person.age} 
-        changed= {(event) => props.changed(event, person.id)} />
-});
-export default persons;
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate ')
+    return true; 
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate ')
+    return {message: 'Snapshot!'};
+  }
+
+  componentDidUpdate() {
+    console.log('[Persons.js] componentDidUpdate ')
+  }
+
+  render() {
+    console.log('[Persons.js] Rendering...')
+
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          changed={event => this.props.changed(event, person.id)}
+        />
+      )
+    })
+  }
+}
+export default Persons
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,72 +1,83 @@
-import React, {Component} from 'react';
-import classes from './App.module.scss';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-
-
+import React, {Component} from 'react'
+import classes from './App.module.scss'
+import Persons from '../components/Persons/Persons'
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+  state = {
+    persons: [
+      {id: 'id001', name: 'Max', age: 28},
+      {id: 'id002', name: 'Mia', age: 12},
+      {id: 'id003', name: 'Rod', age: 27},
+    ],
+    showPersons: false,
+  }
 
-	state = {
-		persons: [
-			{ id:'aza', name: 'Max', age: 28},
-			{ id:'axa', name: 'Mia', age: 12},
-			{ id:'aca', name: 'Rod', age: 27}
-		],
-		showPersons: false
-	}
-	nameChangedHandler = (event, id) => { 
-		const personIndex = this.state.persons.findIndex(p => {
-			return p.id === id;
-		  });
-		const person = {
-			...this.state.persons[personIndex] // prendo persona singola
-		}
-		person.name = event.target.value; // aggiorno con il valore dell'evento
-		const persons =[...this.state.persons]; //copia del vecchio elemento
-		persons[personIndex] = person; // aggiorno la copia del vecchio elemento con il nuovo 
-		this.setState({ persons:persons })// settare stato con il nuovo elemento
-		
-	} 
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivateStateFromProps', props)
+    return state
+  }
+  //   componentWillMount() {
+  //	 preparing state correcticly
+  //     console.log('[App.js] componentWillMount ')
+  //   }
+  componentDidMount() {
+    console.log('[App.js] componentDidMount')
+  }
 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+    const person = {
+      ...this.state.persons[personIndex], // prendo persona singola
+    }
+    person.name = event.target.value // aggiorno con il valore dell'evento
+    const persons = [...this.state.persons] //copia del vecchio elemento
+    persons[personIndex] = person // aggiorno la copia del vecchio elemento con il nuovo
+    this.setState({persons: persons}) // settare stato con il nuovo elemento
+  }
 
-	deletePersonHandler = (personIndex) => {
-		const persons = [...this.state.persons];
-		persons.splice(personIndex,1); 
-		this.setState({persons:persons})
-	}
-	togglePersonsHandler = () => {
-		const doesShow = this.state.showPersons;
-		this.setState({showPersons: !doesShow});
-	}
+  deletePersonHandler = personIndex => {
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
+  }
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow})
+  }
 
+  render() {
+    console.log('[App.js] Render')
+    let persons = null
 
-	render() {
-		let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      )
+    }
 
-		if(this.state.showPersons) {
-			persons =  <Persons 
-						persons={this.state.persons}
-						clicked={this.deletePersonHandler}
-						changed={this.nameChangedHandler}
-						/>
-		
-		}
-		return (			
-			<div className={classes.App}> 
-				<Cockpit 
-				showPersons={this.state.showPersons}
-				persons={this.state.persons}
-				clicked = {this.togglePersonsHandler}
-				titleApp = {this.props.titleApp}
-				/>
-				{persons}
-		    </div>
-		);
-	}
-} 
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+          titleApp={this.props.titleApp}
+        />
+        {persons}
+      </div>
+    )
+  }
+}
 
-export default App;
-
-
-
+export default App
